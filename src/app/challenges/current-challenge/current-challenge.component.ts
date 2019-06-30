3438c6b28a6c1db7fa931b97d084ccf6d2354b3c
Stephen <booklad@gmail.com>
2019-06-30 23:18:59 +0100

    sas

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'ns-current-challenge',
-    templateUrl: './current-challenge.component.html',
+    templateUrl: 'current-challenge.component.html',
     styleUrls: ['current-challenge.component.css'],
     moduleId: module.id
 })
